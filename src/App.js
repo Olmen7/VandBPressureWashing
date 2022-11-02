55ba720850d740c0666f0dddd7f070c6357dbd1a
Olmen Vicente <sethvicente1@gmail.com>
2022-11-02 03:50:58 -0700

    added 404 page and scroll to top component

--- src/App.js
+++ src/App.js
@@ -4,17 +4,20 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Footer } from "./pages/components/Footer";
 import { Navbar } from "./pages/components/Navbar";
 import { Contact } from "./pages/contact/Contact";
+import ScrollToTop from "./ScrollToTop";
 
 function App() {
   return (
     <>
       <Router>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-        <Footer />
+        <ScrollToTop>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+          <Footer />
+        </ScrollToTop>
       </Router>
     </>
   );
